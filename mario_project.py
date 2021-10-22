eeb8a1ac014aeea62949cc0591c5e9c743f21a2c
Claud-Yx <durjs96@gmail.com>
2021-10-23 06:10:57 +0900

    [CAUTION] Update: Threading
    update:
    - add sub thread, 'RenderingTrd'
    -- RenderingTrd: rendering game output
    -- main thread: updating game data

--- mario_project.py
+++ mario_project.py
@@ -3,6 +3,7 @@ from enemy_goomba import *
 from enemy_drybones import *
 from tileset_object import *
 from pico2d import *
+import threading
 import test_keyboard
 
 
@@ -24,6 +25,11 @@ def show_hit_box():
         for box in box_100x100:
             box.hit_box.show_hit_box()
 
+
+def set_fps(fps=60):
+    pass
+
+
 open_canvas()
 
 # Initialization:
@@ -31,7 +37,7 @@ player = Player(250, 140, PS_SUPER)
 goomba = enGoomba(450, 310)
 drybone = enDryBones(350, 335)
 
-# map tile set
+# Map tile set
 box_100x100 = [TileSet(MT_BLOCK100X100) for i in range(16)]
 
 for i in range(0, 8):
@@ -40,41 +46,81 @@ for i in range(0, 8):
 for i in range(8, 16):
     box_100x100[i].set_pos((i-8) * 100 + 50, 250)
 
+# Background
 background = load_image('resource\\background\\castle1.png')
+
+# Debugging virtual keyboard
 test_keyboard.keyboard_init()
 
 Running = True
 
+events = None
+
+def Rendering():
+    global player, goomba, drybone
+    global background
+    global box_100x100
+    global Running
+
+    while Running:
+        print_fps()
+
+        clear_canvas()
+
+        background.draw( 400, 300 )
+
+        for i in range( len( box_100x100 ) ):
+            box_100x100[ i ].draw()
+
+        player.clip_draw()
+        goomba.clip_draw()
+        drybone.clip_draw()
+
+        test_keyboard.update_test_keyboard()
+
+        show_hit_box()
+
+        debug_print(str(get_time()))
+
+        update_canvas()
+
+
+RenderingTrd = threading.Thread( target = Rendering, name = "Update", daemon = True )
+RenderingTrd.start()
+
 # Main Loop:
 while Running:
-    clear_canvas()
 
-    background.draw(400, 300)
+    # clear_canvas()
 
-    for i in range( len( box_100x100 ) ):
-        box_100x100[i].draw()
+    # background.draw(400, 300)
+    #
+    # for i in range( len( box_100x100 ) ):
+    #     box_100x100[i].draw()
 
     player.update()
-    player.clip_draw()
+    # player.clip_draw()
     player.update_frame()
 
     goomba.update()
-    goomba.clip_draw()
+    # goomba.clip_draw()
     goomba.update_frame()
 
     drybone.update()
-    drybone.clip_draw()
+    # drybone.clip_draw()
     drybone.update_frame()
+    #
+    # test_keyboard.update_test_keyboard()
+    #
+    # show_hit_box()
 
-    show_hit_box()
-
-    test_keyboard.update_test_keyboard()
-    update_canvas()
+    # update_canvas()
 
     events = get_events()
     Running = player.handle_event(events)
     test_keyboard.keyboard_handle(events)
 
-    delay(0.05)
+    # set_fps
+    delay(1 / 40)
 
 close_canvas()
