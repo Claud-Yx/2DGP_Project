8d74082fc34cbe099080969be23392201ed4e614
Claud-Yx <durjs96@gmail.com>
2021-10-27 22:52:19 +0900

    Create gs_start_logo.py

---
+++ gs_start_logo.py
@@ -0,0 +1,22 @@
+import gs_framework
+
+def enter():
+    pass
+
+def exit():
+    pass
+
+def handle_events():
+    pass
+
+def pause():
+    pass
+
+def resume():
+    pass
+
+def update():
+    pass
+
+def draw():
+    pass
