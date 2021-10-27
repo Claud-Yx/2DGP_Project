2fd1c6d29d8f04a239d1e2be41a61f02348e94d0
Claud-Yx <durjs96@gmail.com>
2021-10-28 07:19:05 +0900

    Create: gs_edit_state.py

---
+++ gs_edit_state.py
@@ -0,0 +1,25 @@
+from pico2d import *
+
+import gs_framework
+import gs_title
+
+def enter():
+    pass
+
+def exit():
+    pass
+
+def handle_events():
+    pass
+
+def update():
+    pass
+
+def draw():
+    pass
+
+def pause():
+    pass
+
+def resume():
+    pass
