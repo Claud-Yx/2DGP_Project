155da879346af51abc956d7783c6b41006276aa4
Claud-Yx <durjs96@gmail.com>
2021-11-24 02:34:14 +0900

    Create server.py

---
+++ server.py
@@ -0,0 +1,11 @@
+from ob_player import *
+from ob_tileset import *
+from ob_background import *
+from ob_enemy import *
+
+from value import *
+
+player: Player
+enemies: List[Enemy]
+tiles: List[TileSet]
+background: Background
